d066a55fae89a39bcd634aa22984eb240d58a74a
zhouxifeng <410129869@qq.com>
2025-05-13 17:52:45 +0800

    fix

--- telemetry.go
+++ telemetry.go
@@ -1,26 +1,27 @@
 package telemetry
 
 import (
-	"gitlab.ldcu5z28my.com/backend/telemetry/log"
-	"gitlab.ldcu5z28my.com/backend/telemetry/metrics"
+	"github.com/watora/telemetry/log"
+	"github.com/watora/telemetry/metrics"
 )
 
 type Config struct {
-	AppName    string
-	Version    string
-	EndPoint   string
-	UseMetrics bool
-	UseLogger  bool
-	Env        string
+	AppName         string
+	Version         string
+	MetricsEndPoint string
+	LogEndPoint     string
+	UseMetrics      bool
+	UseLogger       bool
+	Env             string
 }
 
 func Init(fn func(cfg *Config)) {
 	cfg := &Config{}
 	fn(cfg)
 	if cfg.UseMetrics {
-		metrics.Init(cfg.AppName, cfg.Version, cfg.Env, cfg.EndPoint)
+		metrics.Init(cfg.AppName, cfg.Version, cfg.Env, cfg.MetricsEndPoint)
 	}
 	if cfg.UseLogger {
-		log.Init(cfg.AppName, cfg.Version, cfg.Env, cfg.EndPoint)
+		log.Init(cfg.AppName, cfg.Version, cfg.Env, cfg.LogEndPoint)
 	}
 }
